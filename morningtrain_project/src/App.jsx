2d42b3d80e0aef271ff6870cf05eeb5beb9d5133
soklope <skp2104@hotmail.com>
2023-05-19 15:01:22 +0200

    Initial setup
    Added SCSS and global colors

--- morningtrain_project/src/App.jsx
+++ morningtrain_project/src/App.jsx
@@ -1,5 +1,6 @@
-import { useState } from 'react'
 import Header from './components/Header'
+import './Styling/GlobalColors.scss'
+import './Styling/MobileNav.scss'
 
 export default function App() {
 
