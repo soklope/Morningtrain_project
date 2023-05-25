bd756cca09868768dcdf3942f984fa4d6ccdeb91
soklope <skp2104@hotmail.com>
2023-05-25 09:14:11 +0200

    Fix store default vlaue

--- morningtrain_project/src/employeeStore.jsx
+++ morningtrain_project/src/employeeStore.jsx
@@ -1,7 +1,12 @@
 import { create } from 'zustand';
 
 const useEmployeeStore = create((set) => ({
-  employee: null,
+  employee: {
+    name: 'John Deez', 
+    tags: ["A", "C"], 
+    workData: [42, 7, 99, 15, 84, 3, 56, 28, 91, 64], 
+    privateData: [52, 71, 42, 8, 16, 89, 63, 77, 26, 81], 
+    position: 'Developer'},
   setEmployee: (employee) => set({ employee }),
 }));
 
